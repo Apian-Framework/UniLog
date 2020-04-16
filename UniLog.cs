13813b88287664137168bbfd7def88586d4556c7
jim berry <jim@bigfattail.com>
2020-04-16 12:10:11 -0400

    After ReSharper inpectcode

--- UniLog.cs
+++ UniLog.cs
@@ -1,6 +1,5 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System;
 
 namespace UniLog
@@ -8,37 +7,41 @@ namespace UniLog
     //
     // UniLogger
     //
+
     public class UniLogger
     {
-        public enum Level 
+        public enum Level
         {
-            Debug = 10,      
-            Verbose = 20,         
+            Debug = 10,
+            Verbose = 20,
             Info = 30,
-            Warn = 40,         
+            Warn = 40,
             Error = 50,
-            Off = 1000,            
+            Off = 1000,
         }
 
         //
         // Statics
         //
-        public static Dictionary<Level, string> LevelNames = new Dictionary<Level, string>()
+
+        // Public API
+        // ReSharper disable MemberCanBePrivate.Global,UnusedMember.Global,FieldCanBeMadeReadOnly.Global
+        public static Dictionary<Level, string> LevelNames {get;} = new Dictionary<Level, string>()
         {
             {Level.Debug, "Debug"},
-            {Level.Verbose, "Verbose"},            
+            {Level.Verbose, "Verbose"},
             {Level.Info, "Info"},
-            {Level.Warn, "Warn"},                                                  
-            {Level.Error, "Error"},         
-            {Level.Off, "Off"},              
+            {Level.Warn, "Warn"},
+            {Level.Error, "Error"},
+            {Level.Off, "Off"},
         };
 
         // 0 = name
         // 1 = level
         // 2 = message
 
-        public static Level defaultLevel = Level.Warn;
-        public static bool defaultThrowOnError = false;
+        public static Level DefaultLevel = Level.Warn;
+        public static bool DefaultThrowOnError = false;
 
         public static UniLogger GetLogger(string name)
         {
@@ -56,7 +59,7 @@ namespace UniLog
 
 #if UNITY_2019_1_OR_NEWER
 
-        public const string defaultFormat = "{1}: {2}";
+        public string DefaultFormat = "{1}: {2}";
 
         //
         // Unity Implementation
@@ -67,22 +70,22 @@ namespace UniLog
         public UniLogger(string name)
         {
             Name = name;
-            LogLevel = defaultLevel;
-            LogFormat = defaultFormat;
-            ThrowOnError = defaultThrowOnError;
-            unityLogger  = new UnityEngine.Logger(UnityEngine.Debug.unityLogger.logHandler);            
+            LogLevel = DefaultLevel;
+            LogFormat = DefaultFormat;
+            ThrowOnError = DefaultThrowOnError;
+            unityLogger  = new UnityEngine.Logger(UnityEngine.Debug.unityLogger.logHandler);
         }
 
-        protected void _Write(string name, Level lvl, string msg)
+        private void _Write(string name, Level lvl, string msg)
         {
             if (lvl >= LogLevel)
             {
-                string outMsg = string.Format(LogFormat, name, LevelNames[lvl], msg);                
+                string outMsg = string.Format(LogFormat, name, LevelNames[lvl], msg);
                 //string outMsg = string.Format(LogFormat, LevelNames[lvl], msg);
                 switch (lvl)
                 {
                 case Level.Debug:
-                case Level.Verbose:                
+                case Level.Verbose:
                 case Level.Info:
                     unityLogger.Log($"{name}: {outMsg}");
                     break;
@@ -94,8 +97,8 @@ namespace UniLog
                         throw new Exception($"{name}: {outMsg}");
                     else
                         unityLogger.LogError(name, outMsg);
-                    break;   
-                }             
+                    break;
+                }
             }
         }
 
@@ -104,17 +107,17 @@ namespace UniLog
         // Non-unity
         //
 
-        public const string defaultFormat = "[{0}] {1}: {2}";
+        public string DefaultFormat = "[{0}] {1}: {2}";
 
         public UniLogger(string name)
         {
             Name = name;
-            LogLevel = defaultLevel;
-            LogFormat = defaultFormat;
-            ThrowOnError = defaultThrowOnError;
+            LogLevel = DefaultLevel;
+            LogFormat = DefaultFormat;
+            ThrowOnError = DefaultThrowOnError;
         }
 
-        protected void _Write(string name, Level lvl, string msg)
+        private void _Write(string name, Level lvl, string msg)
         {
             if (lvl >= LogLevel)
             {
@@ -128,52 +131,52 @@ namespace UniLog
         }
 
 #endif
-        // Instance
-        public string Name {get; private set;}
-        public Level LogLevel {get; set;}            
-        public string LogFormat {get; set;}
 
-        public bool ThrowOnError {get; set;}
+        // Instance
+        public string Name {get;}
+        public Level LogLevel;
+        public string LogFormat;
+        public bool ThrowOnError;
 
         public void Info(string msg) => _Write(Name, Level.Info, msg);
-        public void Verbose(string msg) => _Write(Name, Level.Verbose, msg);        
-        public void Debug(string msg) => _Write(Name, Level.Debug, msg);        
+        public void Verbose(string msg) => _Write(Name, Level.Verbose, msg);
+        public void Debug(string msg) => _Write(Name, Level.Debug, msg);
         public void Warn(string msg) => _Write(Name, Level.Warn, msg);
-        public void Error(string msg) => _Write(Name, Level.Error, msg);                
+        public void Error(string msg) => _Write(Name, Level.Error, msg);
 
+        // End  API
+        // ReSharper enable MemberCanBePrivate.Global,UnusedMember.Global,FieldCanBeMadeReadOnly.Global
 
     }
 
     public class UniLoggerCollection
     {
-        protected Dictionary<string, UniLogger> loggers;
+        private readonly Dictionary<string, UniLogger> _loggers;
 
         //
         // Yeah, it's a singleton
         //
-        private static UniLoggerCollection instance = null;
+        private static UniLoggerCollection _instance;
+
         public static UniLoggerCollection GetInstance()
         {
-            if (instance == null)
-            {
-                instance = new UniLoggerCollection();
-            }
-            return instance;
-        }   
-
-        protected UniLoggerCollection()
-        {
-            loggers = new Dictionary<string, UniLogger>();
+            _instance = _instance ?? new UniLoggerCollection();
+            return _instance;
         }
         public static UniLogger GetLogger(string name)
         {
             UniLoggerCollection inst = GetInstance();
-            return inst.loggers.ContainsKey(name) ? inst.loggers[name] : inst.AddLogger(name);
+            return inst._loggers.ContainsKey(name) ? inst._loggers[name] : inst.AddLogger(name);
         }
 
-        protected  UniLogger AddLogger(string name)
+        private UniLoggerCollection()
         {
-            return loggers[name] = new UniLogger(name);
+            _loggers = new Dictionary<string, UniLogger>();
+        }
+
+        private  UniLogger AddLogger(string name)
+        {
+            return _loggers[name] = new UniLogger(name);
         }
 
     }
